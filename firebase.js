16101bc7a09f3d6b52bf42ee58e0caa63733dac7
Emirdzhan Syuleymanoglu | Bined B.V. <emirdzhan@bined.nl>
2025-10-05 21:44:55 +0200

    Add firebase.js configuration

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARlOXg2YuKrEsRWARCUTiabHoMN1hO3Ks",
+  authDomain: "shiftpilot-b3c1d.firebaseapp.com",
+  projectId: "shiftpilot-b3c1d",
+  storageBucket: "shiftpilot-b3c1d.firebasestorage.app",
+  messagingSenderId: "676810685497",
+  appId: "1:676810685497:web:db3162bc7a394442caa1e8",
+  measurementId: "G-BEJ85FBSCZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
